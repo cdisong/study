898b4635cee0c46d691fc663943615bf16b68bb1
Christine Song <christinedisong@gmail.com>
2018-03-15 23:28:14 -0700

    add transpose & pseudocode

--- jumpstart_a_2.rb
+++ jumpstart_a_2.rb
@@ -16,7 +16,49 @@ def missing_numbers(arr)
   puts missing_numbers([-5, -1, 0, 3]) == [-4, -3, -2, 1, 2]
   puts missing_numbers([0, 5]) == [1, 2, 3, 4]
   
-  
+  # Transpose
+# You are given a n * n 2D matrix
+# Example:
+# Matrix = [[1, 2],
+#           [3, 4]]
+# Write a method that will transpose a matrix. The transpose of a matrix is
+# obtained by turning all of the columns of a matrix into the rows and vice versa.
+# So an element at index ij would be at index ji once tranposed.
+# For example:
+# transpose(Matrix) = [[1, 3],
+#                      [2, 4]]
+#
+# You may assume a square matrix as input. Do not use `.transpose`
+def transpose(matrix)
+  #
+  # your code goes here
+  # start with empty array, create new array 
+  # double loop through matrix, and switch/shovel, arr[i][j] = result[j][i]
+end
+# puts "-------Transpose-------"
+# matrix_one = [[1, 2],
+#             [3, 4]]
+            
+# matrix_two = [[1, 4, 7],
+#             [2, 5, 8],
+#             [3, 6, 9]]
+            
+# matrix_three = [[1, 2, 3, 4],
+#               [5, 6, 7, 8],
+#               [9, 10, 11, 12],
+#               [13, 14, 15, 16]]
+              
+# puts transpose(matrix_one) == [[1, 3],
+#                              [2, 4]]
+                             
+# puts transpose(matrix_two) == [[1, 2, 3],
+#                              [4, 5, 6],
+#                              [7, 8, 9]]
+                             
+# puts transpose(matrix_three) == [[1, 5, 9, 13],
+#                                [2, 6, 10, 14],
+#                                [3, 7, 11, 15],
+#                                [4, 8, 12, 16]]
   
   ### Titleize
   #
