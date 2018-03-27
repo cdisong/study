09c973a3d43389916d46ad6ddde826726b448d54
Christine Song <christinedisong@gmail.com>
2018-03-26 21:51:26 -0700

    prep for jumpstart

---
+++ react_intro/dummystateforhack.jsx
@@ -0,0 +1,30 @@
+const dummyState = [
+    {
+      “faceRectangle”: {
+        “top”: 258,
+        “left”: 283,
+        “width”: 215,
+        “height”: 215
+      },
+      “faceAttributes”: {
+        “emotion”: {
+          “anger”: 0,
+          “contempt”: 0.003,
+          “disgust”: 0,
+          “fear”: 0,
+          “happiness”: 0.102,
+          “neutral”: 0.797,
+          “sadness”: 0.001,
+          “surprise”: 0.097
+        }
+      }
+    }
+]
+  
+
+
+state = { 
+  emotions: [],
+  
+}
+
