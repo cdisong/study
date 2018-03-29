91acaabf71e3ce12ad12f987464fdb19c1d2ef21
Christine Song <christinedisong@gmail.com>
2018-03-28 17:08:20 -0700

    even splitters

--- step2demo.rb
+++ step2demo.rb
@@ -29,4 +29,53 @@ def hipsterfy(sentence)
    
    
    
-   
+# Even Splitters
+
+# Return an array of characters on which we can split an input string to produce
+# substrings of the same length.
+
+# Don't count empty strings after the split.
+
+# Here's an example for "banana":
+
+# "banana".split("a") # => ["b", "n", "n"] (all elements same length)
+# "banana".split("b") # => ["", anana"] (all elements same length - there's only
+# one element "anana" because the empty string doesn't count)
+# "banana".split("n") # => ["ba", "a", "a"] (all elements NOT same length)
+
+# result: ["b", "a"]
+
+def even_splitters(string)
+  uniq_letters = string.chars.uniq
+  result = []
+
+  uniq_letters.each do |letter|
+    result << letter if even_split?(letter,string)
+  end
+
+  result
+end
+
+def even_split?(letter, string)
+  sub_array = string.split(letter)
+  sub_array2 = []
+
+  sub_array.each do |el|
+    sub_array2 << el if el.length > 0
+  end
+  
+  sub_array2.each do |el|
+    return false if el.length != sub_array2[0].length
+  end
+
+  true
+end
+
+
+# puts "-----Even Splitters----"
+puts even_splitters("") == []
+puts even_splitters("t") == ["t"]
+puts even_splitters("jk") == ["j", "k"]
+puts even_splitters("xoxo") == ["x", "o"]
+puts even_splitters("banana") == ["b","a"]
+puts even_splitters("mishmash") == ["m","h"]   
