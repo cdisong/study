5ff85ce21acdcb31aa3f48701c7e2cdcd634abc3
Christine Song <christinedisong@gmail.com>
2018-03-29 19:27:28 -0700

    add assessment demo solutions

--- step2demo.rb
+++ step2demo.rb
@@ -114,3 +114,106 @@ puts missing_numbers([1, 3]) == [2]
 puts missing_numbers([2, 3, 7]) == [4, 5, 6]
 puts missing_numbers([-5, -1, 0, 3]) == [-4, -3, -2, 1, 2]
 puts missing_numbers([0, 5]) == [1, 2, 3, 4]
+
+# Reverse Digits
+
+# Define a method, #reverse_digits(int), that reverses the digits of its 
+# argument and returns the resulting number.
+def reverse_digits(int) # 1
+  int.to_s.reverse.to_i 
+end
+
+puts "-------Reverse Digits-------"
+puts reverse_digits(1) == 1
+puts reverse_digits(123) == 321
+puts reverse_digits(1005) == 5001
+puts reverse_digits(12345678) == 87654321
+
+# ------------------------------------------------------------------------------
+
+# Thirds Product
+
+# Define a method, #thirds_product(array), that accepts an array of integers 
+# as an argument. Your method should return the product of every third 
+# element multiplied together. It should return 1 if there is no third 
+# element.
+
+# ex: thirds_product([0, 1, 2, 3, 4, 5, 6, 7, 8]) ==> 2 * 5 * 8 == 80
+
+def thirds_product(array)
+  product = 1
+  i = 2
+  while i < array.length
+    product *= array[i]
+    i += 3
+  end
+  product
+end
+
+puts "-------Thirds Product-------"
+puts thirds_product([1, 2]) == 1
+puts thirds_product([1, 2, 0]) == 0
+puts thirds_product([0, 1, 2, 3, 4, 5]) == 10
+puts thirds_product([1, 2, 5, 3, 4, 2, 6, 4, 2]) == 20
+
+# ------------------------------------------------------------------------------
+
+# How Many Likes?
+
+# Define a method, #how_many_likes?(sentence) that accepts a string as an 
+# argument. Your method should count the number of times the sentence uses 
+# the word "like".
+
+def how_many_likes?(sentence)
+  count = 0
+  words = sentence.split(" ")
+  
+  words.each do |word|
+    count += 1 if word == "like"
+  end
+  
+  count 
+end
+
+puts "-------How Many Likes?-------"
+puts how_many_likes?("this sentence is just fine") == 0
+puts how_many_likes?("i really like ruby") == 1
+puts how_many_likes?("i like really like how ruby works") == 2
+puts how_many_likes?("this is like totally like too many like words") == 3
+
+# ------------------------------------------------------------------------------
+
+# Pair Product?
+
+# Define a boolean method, #pair_product?, that accepts two arguments: an 
+# array of integers and a target_product (an integer). The method returns a 
+# boolean indicating whether any pair of elements in the array multiplied 
+# together equals that product. You cannot multiply an element by itself. 
+# An element on its own is not a product.
+
+def pair_product?(arr, target_product)
+  
+  i = 0
+  while i < arr.length - 1
+    j = i + 1
+    while j < arr.length
+      if arr[i] * arr[j] == target_product
+        return true 
+      end
+      j += 1
+    end 
+    i += 1
+  end
+  
+  false
+  
+  arr.combination(2).any? {|a,b| a*b == target_product}
+  # !!combo.detect {|a,b| a*b == target_product}
+end
+
+puts "-------Pair Product?-------"
+
+puts pair_product?([5, 3, 4, 2], 6) == true
+puts pair_product?([5, 3, 5], 10) == false
+puts pair_product?([6, 1, 7, 8], 1) == false
+puts pair_product?([2, 8, 6, 2], 4) == true
