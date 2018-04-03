f2e100413e4ec11608dec9515e1738c4ae648fcd
Christine Song <christinedisong@gmail.com>
2018-04-03 14:46:54 -0700

    add demo step 2

--- step2demo.rb
+++ step2demo.rb
@@ -253,3 +253,31 @@ puts vigenere_cipher("toerrishuman", [1]) == "upfssjtivnbo"
 puts vigenere_cipher("toerrishuman", [1, 2]) == "uqftsktjvobp"
 puts vigenere_cipher("toarrispirate", [1, 2, 3, 4]) == "uqdvskvtjtdxf"
 puts vigenere_cipher("zzz", [1, 2, 1]) === "aba"
+
+
+# Compound Words
+
+# Define a method, #compound_words?(word_list, target), that accepts 2 arguments:
+# an array of words and a target, compound word. Check to see if any two words
+# in the list can be combined to make the compound word. Don't double-use words.
+
+def compound_words?(array, target)
+  i = 0 
+  while i < array.length - 1 
+    j = 0 
+    while j < array.length 
+      if array[i] + array[j] == target && i != j 
+        return true 
+      end 
+      j += 1 
+    end
+    i += 1 
+  end 
+  return false 
+end
+
+puts "------Compound Words------"
+puts compound_words?(["ice", "cream"], "icecream") == true
+puts compound_words?(["cake", "candle", "cup", "wish"], "cupcake") == true
+puts compound_words?(["hello", "bye", "greetings"], "byebye") == false
+puts compound_words?(["bye", "bye", "greetings"], "byebye") == true
