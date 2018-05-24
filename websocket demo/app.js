79faf980941250970b502d0d46a2bde72e4bb079
Christine Song <christinedisong@gmail.com>
2018-05-24 11:19:56 -0700

    initialize websocket code

---
+++ websocket demo/app.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+
+    // references to elements on the page.
+    var form = document.getElementById('message-form');
+    var messageField = document.getElementById('message');
+    var messagesList = document.getElementById('messages');
+    var socketStatus = document.getElementById('status');
+    var closeBtn = document.getElementById('close');
+  
+  };
