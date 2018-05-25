ad83805c2c8727b6c171a46b53a39e566854cc93
Christine Song <christinedisong@gmail.com>
2018-05-25 11:23:02 -0700

    create new instance of websocket and add event listener for establishing connection

--- websocket demo/app.js
+++ websocket demo/app.js
@@ -6,5 +6,15 @@ window.onload = function() {
     var messagesList = document.getElementById('messages');
     var socketStatus = document.getElementById('status');
     var closeBtn = document.getElementById('close');
-  
+    
+    // create new websocket
+    var socket = new WebSocket('ws://echo.websocket.org');
+
+    // when websocket opens, show connected message 
+    socket.onopen = function(event) {
+        socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.url; 
+        socketStatus.className = 'open'; 
+    };
+
+    
   };
