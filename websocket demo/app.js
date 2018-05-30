42a298d1bf34b3a9ebc4928c2d961b46b3f2ca7a
Christine Song <christinedisong@gmail.com>
2018-05-30 11:29:26 -0700

    message submit websocket

--- websocket demo/app.js
+++ websocket demo/app.js
@@ -16,9 +16,24 @@ window.onload = function() {
         socketStatus.className = 'open'; 
     };
 
+    // handle any errors that occur 
     socket.onerror = function(error) {
         console.log('WebSocket Error: ' + error);
     };
 
+    // send a message when the form is submitted 
+    form.onsubmit = function(e) {
+        e.preventDefault();
+        // retreive message from text area 
+        var message = messageField.value;
+        // send message through websocket
+        socket.send(message); 
+        // add message to messages list 
+        messagesList.innerHTML += '<li class="sent"><span>Sent:</span>' + message '</li>';
+        // clear out message field
+        messageField.value = ''; 
+        
+        return false;
+    }   
     
   };
